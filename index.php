246e1fc57ce02d87a5b7054dc05de9939cc31855
Raven Barrogo <barrogoraven@gmail.com>
2024-11-25 10:33:32 +0800

    ADD: .htaccess :pencil:

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 $title = "Home";
 ?>
 
-
 <!doctype html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
   <title><?= $title ?></title>
 </head>
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import flowbitePlugin from 'flowbite/plugin'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['*.php', './src/**/*.{js,ts,jsx,tsx}', './node_modules/flowbite/**/*.js'],
+  content: ['*.php', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
