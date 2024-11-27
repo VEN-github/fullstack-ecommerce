da93408ddc3d2579480685a228be186e108430a0
Raven Barrogo <barrogoraven@gmail.com>
2024-11-27 18:08:22 +0800

    CHANGE: file case :hammer:

--- public/index.php
+++ public/index.php
@@ -6,6 +6,6 @@ use App\Core\Application;
 
 $app = new Application(dirname(__DIR__) . '/app');
 
-require_once Application::$ROOT_DIR . '/routes/routes.php';
+require_once Application::$ROOT_DIR . '/Routes/routes.php';
 
 $app->run();
