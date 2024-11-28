0db46b153785c43e49f17643eb7723203a9c7e4c
Raven Barrogo <barrogoraven@gmail.com>
2024-11-28 11:01:58 +0800

    CHANGE: partials :art:

--- app/views/layouts/admin.php
+++ app/views/layouts/admin.php
@@ -3,22 +3,7 @@
 use App\Core\Application;
 ?>
 
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-  <link rel="shortcut icon" href="/favicon.ico" />
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <meta name="apple-mobile-web-app-title" content="INVI" />
-  <link rel="manifest" href="/site.webmanifest" />
-  <?= vite('src/main.ts') ?>
-  <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js"></script>
-  <title><?= $title ?? 'INVI' ?></title>
-</head>
+<?php require_once Application::$ROOT_DIR . '/views/partials/admin/header.php'; ?>
 
 <body>
 
