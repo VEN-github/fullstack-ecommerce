46acc5deee43fa2f8422ff6ad729f86a1a98b7f3
Raven Barrogo <barrogoraven@gmail.com>
2024-12-05 12:13:47 +0800

    Delete app directory

--- app/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->safeLoad();
--- app/routes/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Core\Application;
-
-require_once  Application::$ROOT_DIR . '/Routes/Admin/index.php';
-require_once  Application::$ROOT_DIR . '/Routes/Client/index.php';
