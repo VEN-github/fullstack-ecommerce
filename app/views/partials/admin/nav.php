b87e96da76e35da0405f40a867099c4ce84c4ee4
Raven Barrogo <barrogoraven@gmail.com>
2024-12-09 18:55:08 +0800

    CHANGE: z-index of sidebar and nav :art:

--- app/views/partials/admin/nav.php
+++ app/views/partials/admin/nav.php
@@ -6,7 +6,7 @@ $admin = Application::$app->admin;
 ?>
 
 <nav
-  class="fixed left-0 right-0 top-0 z-50 border-b border-gray-200 bg-white px-4 py-2.5">
+  class="fixed left-0 right-0 top-0 z-20 border-b border-gray-200 bg-white px-4 py-2.5">
   <div class="flex flex-wrap items-center justify-between">
     <div class="flex items-center justify-start">
       <button
--- app/views/partials/admin/sidebar.php
+++ app/views/partials/admin/sidebar.php
@@ -1,5 +1,5 @@
 <aside
-  class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full border-r border-gray-200 bg-white pt-14 transition-transform dark:border-gray-700 dark:bg-gray-800 md:translate-x-0"
+  class="fixed left-0 top-0 z-10 h-screen w-64 -translate-x-full border-r border-gray-200 bg-white pt-14 transition-transform dark:border-gray-700 dark:bg-gray-800 md:translate-x-0"
   aria-label="Sidenav"
   id="drawer-navigation">
   <div class="h-full overflow-y-auto bg-white px-3 py-5">
